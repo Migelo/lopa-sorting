4ecb22408daf666898f91b8f2ef0e9e7a0d4275a
Miha <miha@filetki.si>
2016-08-18 16:19:41 +0200

    Exported the function to merge mdisp files from mdispCompare to mergeMdisp.

---
+++ mergeMdisp.py
@@ -0,0 +1,16 @@
+import numpy as np
+import glob
+import argparse
+
+parser = argparse.ArgumentParser(description='Merge .mdisp files.')
+parser.add_argument('folder', metavar='SPECTRA', help='Folder with the mdisp files.', type=str)
+parser.add_argument('outputFile', type=str, help='Set the output file.')
+args = parser.parse_args()
+
+file_list = sorted(glob.glob(args.folder + '/*.mdisp'))
+
+a = np.array([0,0])
+for filee in file_list:
+    a = np.vstack([a,np.loadtxt(filee)])
+a = np.delete(a, (0), axis=0)
+np.savetxt(args.outputFile, a, fmt = '%.7e')
